f54b695e050eb4bf8551211230896000dc21e643
arif3188 <48197205+arif3188@users.noreply.github.com>
2019-03-05 16:32:16 +0530

    changes done

--- test.java
+++ test.java
@@ -2,6 +2,8 @@ public  class test{
 	public static void main(String [] args)
 		{
 		System.out.println("Hello this is test file");
+		System.out.println("Hi this is test file");
+		System.out.println("Hi this is test file");
 		
 }
 }
