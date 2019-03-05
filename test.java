8c220330ec8103bbb0fb79e2e924c23cb6e12801
arif3188 <48197205+arif3188@users.noreply.github.com>
2019-03-05 16:27:20 +0530

    added hi test file statement

--- test.java
+++ test.java
@@ -1,6 +1,8 @@
 public  class test{
 	public static void main(String [] args)
 		{
-		System.out.println("Hello this is test file");		
+		System.out.println("Hello this is test file");
+		System.out.println("Hi this is test file");	
+		
 }
 }
